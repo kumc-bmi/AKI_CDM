7c927ef1b7a891f29efdbad1f85f255046dffa3f
sxinger <sxingerl@gmail.com>
2018-07-20 22:11:29 -0500

    start writing functions for extracting AKI cohort based off PCORNET CDM

---
+++ create_AKI_cohort.R
@@ -0,0 +1,15 @@
+.create_AKI_Cohort<-function(conn,
+                             cdm_db_schema,
+                             oracle_tmp_schema,
+                             output){
+  ##inclusion criteria
+  #sql...
+  
+  ##exclusion criteria
+  #sql...
+  
+  ##final cohort
+  #sql...
+  
+  return()
+}
