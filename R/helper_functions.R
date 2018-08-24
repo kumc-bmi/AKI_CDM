749d37432a06121692732b2680a7f02b75cad834
sxinger <sxingerl@gmail.com>
2018-08-23 19:33:02 -0500

    merge all helper functions together

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,9 +1,5 @@
 ##---------------------------helper functions--------------------------------------##
-
-#########################
-#all about initial setup#
-#########################
-#install (if needed) and require packages
+## install (if needed) and require packages
 require_libraries<-function(package_list){
   #install missing packages
   new_packages<-package_list[!(package_list %in% installed.packages()[,"Package"])]
@@ -19,7 +15,31 @@ require_libraries<-function(package_list){
 
 
 
+## parse Oracle sql lines
+parse_sql<-function(file_path){
+  #read file
+  con<-file(file_path,"r")
+  #initialize string
+  sql.string <- ""
+  
+  while (TRUE){
+    #parse the first line
+    line <- readLines(con, n = 1)
+    #check for endings
+    if (length(line)==0) break
+    #check for overhead comments
+    if(grepl("^(/\\*)",line)) next
+    #remove the first line
+    line<-gsub("\\t", " ", line)
+    #translate comment symbol '--'
+    if(grepl("--",line) == TRUE){
+      line <- paste(sub("--","/*",line),"*/")
+    }
+    #attach new line
+    sql.string <- paste(sql.string, line)
+  }
+  close(con)
+  return(sql.string)
+}
 
-######################
-#Exploratory Analysis#
-######################
+## 
