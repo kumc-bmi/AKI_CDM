53fd8b38a43cc1975379a8a156e0b44a9f5815db
sxinger <sxingerl@gmail.com>
2018-08-28 00:26:07 -0500

    change variable table output to long skinny format

--- AKI_CDM_EXT_VALID_p1_QA.R
+++ AKI_CDM_EXT_VALID_p1_QA.R
@@ -35,23 +35,26 @@ save(Table1,file="./data/Table1.Rdata")
 
 #print out attrition table
 print(cohort$attrition)
-##pass tests!
+#Todo: consort diagram
 
-
-
-#collect variables
+#collectand summarize variables
 load("./data/Table1.Rdata")
-Table1 %<>%
-  dplyr::mutate(ADMIT_DATE=as.Date(as.character(ADMIT_DATE),"%Y-%m-%d"),
-                DISCHARGE_DATE=as.Date(as.character(DISCHARGE_DATE),"%Y-%m-%d"),
-                AKI1_ONSET=as.Date(as.character(AKI1_ONSET),"%Y-%m-%d"),
-                AKI2_ONSET=as.Date(as.character(AKI2_ONSET),"%Y-%m-%d"),
-                AKI3_ONSET=as.Date(as.character(AKI3_ONSET),"%Y-%m-%d"))
-
+# auxilliary summaries and tables
 enc_tot<-length(unique(Table1$ENCOUNTERID))
+# critical dates of AKI encounters
+aki_stage_ind<-Table1 %>%
+  dplyr::select(PATID, ENCOUNTERID, ADMIT_DATE, DISCHARGE_DATE,
+                NONAKI_ANCHOR, AKI1_ONSET,AKI2_ONSET,AKI3_ONSET) %>%
+  gather(chk_pt, critical_date, -ENCOUNTERID) %>%
+  filter(!is.na(critical_date)) %>%
+  mutate(chk_pt=gsub("_.*","",chk_pt)) %>%
+  group_by(chk_pt) %>%
+  dplyr::mutate(stg_tot_cnt=n()) %>%
+  ungroup %>%
+  arrange(PATID, ENCOUNTERID, chk_pt, critical_date, stg_tot_cnt)
 
 
-#demographic
+## demographic
 demo<-dbGetQuery(conn,
                  parse_sql("./inst/collect_demo.sql",
                            cdm_db_schema=cdm_db_schema)$statement) %>%
@@ -60,44 +63,24 @@ demo<-dbGetQuery(conn,
                            AGE >= 36 & AGE <= 45 ~ "36-45",
                            AGE >= 46 & AGE <= 55 ~ "46-55",
                            AGE >= 56 & AGE <= 65 ~ "56-65",
-                           AGE >= 66 ~ "66<="))
-
+                           AGE >= 66 ~ "66<=")) %>%
+  dplyr::select(PATID,ENCOUNTERID,
+                AGE,AGE_GRP,SEX,RACE,HISPANIC,DDAYS_SINCE_ENC) %>%
+  gather(key,value,-PATID,-ENCOUNTERID)
 #save
 save(demo,file="./data/AKI_demo.Rdata")
 
-#illogical dates (aki onsets before birth or after death)
-aki_bad_dates<-aki_stage_ind<-tbl1 %>%
-  dplyr::select(ENCOUNTERID,
-                AKI1_ONSET,AKI2_ONSET,AKI3_ONSET) %>%
-  gather(aki_stg, onset, -ENCOUNTERID) %>%
-  filter(!is.na(onset)) %>%
-  dplyr::select(ENCOUNTERID, aki_stg)%>%
-  mutate(aki_stg=gsub("_.*","",aki_stg)) %>%
-  
-
-
-
 #summaries
-demo_summ<-aki_stage_ind<-tbl1 %>%
-  dplyr::select(ENCOUNTERID,
-                AKI1_SINCE_ADMIT,AKI2_SINCE_ADMIT,AKI3_SINCE_ADMIT) %>%
-  gather(aki_stg, days_since, -ENCOUNTERID) %>%
-  filter(!is.na(days_since)) %>%
-  dplyr::select(ENCOUNTERID, aki_stg)%>%
-  mutate(aki_stg=gsub("_.*","",aki_stg)) %>%
-  group_by(aki_stg) %>%
-  dplyr::mutate(stg_tot_cnt=n()) %>%
-  ungroup %>%
-  arrange(ENCOUNTERID, aki_stg, stg_tot_cnt) %>%
-  left_join(demo %>%
-              dplyr::select(-AGE) %>%
-              gather(demo_type,demo_val,-PATID, -ENCOUNTERID), 
+demo_summ<-aki_stage_ind %>% 
+  filter(!chk_pt %in% c("ADMIT","DISCHARGE")) %>%
+  dplyr::select(-critical_date) %>%
+  left_join(demo %>% filter(!(key %in% c("AGE","DDAYS_SINCE_ENC"))), 
             by="ENCOUNTERID") %>%
-  group_by(aki_stg,stg_tot_cnt,demo_type,demo_val) %>%
+  group_by(chk_pt,stg_tot_cnt,demo_type,demo_val) %>%
   dplyr::summarize(enc_cnt = n(),
                    enc_prop = round(n()/stg_tot_cnt[1],2)) %>%
   ungroup %>% dplyr::select(-stg_tot_cnt) %>%
-  gather(summ,summ_val,-aki_stg,-demo_type,-demo_val) %>%
+  gather(summ,summ_val,-chk_pt,-demo_type,-demo_val) %>%
   # decode demo_val
   left_join(meta %>% dplyr::select(COLUMN_NAME,VAR_CODE,VAR_NAME),
             by=c("demo_type"="COLUMN_NAME","demo_val"="VAR_CODE")) %>%
@@ -106,18 +89,103 @@ demo_summ<-aki_stage_ind<-tbl1 %>%
   unite("demo_type_cat",c("demo_type","demo_val")) %>%
   # attach totals at bottom
   bind_rows(aki_stage_ind %>%
-              dplyr::select(aki_stg,stg_tot_cnt) %>% 
+              dplyr::select(chk_pt,stg_tot_cnt) %>% 
               unique %>% 
               dplyr::rename(enc_cnt=stg_tot_cnt) %>%
               mutate(enc_prop=round(enc_cnt/117453,2),
                      demo_type_cat="TOTAL(%/overall)") %>%
-              gather(summ,summ_val,-aki_stg,-demo_type_cat) %>%
-              dplyr::select(aki_stg,demo_type_cat,summ,summ_val)) %>%
-  unite("stg_summ",c("aki_stg","summ")) %>%
+              gather(summ,summ_val,-chk_pt,-demo_type_cat) %>%
+              dplyr::select(chk_pt,demo_type_cat,summ,summ_val)) %>%
+  unite("stg_summ",c("chk_pt","summ")) %>%
   unique %>% spread(stg_summ,summ_val) %>%
   replace(.,is.na(.),0)
+View(demo_summ)
+
+
+## vital
+#vital: HT,WT,BMI
+vital<-dbGetQuery(conn,
+                  parse_sql("./inst/collect_vital.sql",
+                            cdm_db_schema=cdm_db_schema)$statement) %>%
+  mutate(BMI_GRP = case_when(ORIGINAL_BMI <= 25 ~ "BMI <= 25",
+                             ORIGINAL_BMI > 25 &  ORIGINAL_BMI <= 30 ~ "BMI 26-30",
+                             ORIGINAL_BMI >=31  ~ "BMI >= 31"))
+  gather(key,value,-PATID,-ENCOUNTERID,-VITALID0)
+#save
+save(vital,file="./data/AKI_vital.Rdata")
+
+
+# collect summaries
+ht_wt_bmi_summ<-ht_wt_bmi %>%
+  dplyr::select(PATID, HT, WT) %>%
+  dplyr::filter(!is.na(HT) | !is.na(WT)) %>%
+  left_join(aki_stage_idx, by="PATID") %>%
+  dplyr::summarize(HT_records=sum(!is.na(HT)),
+                   HT_mean=mean(HT,na.rm=T),
+                   HT_median=median(HT,na.rm=T),
+  )
+
+ht_wt_bmi<-vital %>% 
+  dplyr::select(PATID, VITALID, MEASURE_DATE_TIME,
+                HT,WT,ORIGINAL_BMI) %>%
+  dplyr::filter(!is.na(HT) & !is.na(WT)) %>%
+  left_join(demo %>% dplyr::select(PATID,AGE_GRP,SEX) %>% unique,
+            by="PATID") %>%
+  gather(key,val,-PATID,-age_grp,-SEX,-VITALID,-MEASURE_DATE_TIME) %>%
+  group_by(age_grp,SEX,key) %>%
+  dplyr::mutate(low_bd=ifelse(key=="ORIGINAL_BMI",
+                              pmax(10,median(val,na.rm=T)-3*IQR(val,na.rm=T,type=8)),
+                              pmax(50,median(val,na.rm=T)-3*IQR(val,na.rm=T,type=8))),   
+                up_bd=pmin(500,median(val,na.rm=T)+4.5*IQR(val,na.rm=T,type=8))) %>% 
+  ungroup %>%
+  dplyr::mutate(outlier_ind = ifelse(val > up_bd | val < low_bd, 1,0))
+
+#outliers summaries
+ht_wt_bmi %>% filter(outlier_ind==1) %>%
+  group_by(age_grp,SEX,key,low_bd,up_bd) %>%
+  dplyr::summarize(pat_cnt=length(unique(PATID))) %>% 
+  ungroup %>% unique %>% arrange(age_grp,SEX,key) %>%
+  View
+
+#vital: SBP, DBP
+#detect outliers - SYSTOLIC, DIASTOLIC, at encounter level
+# AKI_VITAL_test<-AKI_VITAL %>% dplyr::filter(PATID < 100)
+# scalability issue!
+bp<-AKI_VITAL %>% 
+  dplyr::select(ENCOUNTERID,VITALID, MEASURE_DATE_TIME,
+                SYSTOLIC, DIASTOLIC) %>%
+  dplyr::filter(!is.na(SYSTOLIC) & !is.na(DIASTOLIC)) %>%
+  gather(key,val,-ENCOUNTERID,-VITALID, -MEASURE_DATE_TIME) %>%
+  group_by(key,ENCOUNTERID) %>%
+  dplyr::mutate(measure_time=rank(MEASURE_DATE_TIME),
+                freq=n()) %>%
+  dplyr::mutate(loess_fit=predict(loess(val~measure_time),se=T)$fit,
+                loess_se=predict(loess(val~measure_time),se=T)$se) %>%
+  dplyr::mutate(low_bd=loess_fit-1.5*sqrt(freq)*loess_se,
+                up_bd=loess_fit+1.5*sqrt(freq)*loess_se) %>%
+  ungroup %>%
+  dplyr::mutate(outlier_ind = ifelse(val > up_bd | val < low_bd, 1,0))
+
+
+#outliers summaries
+bp %>% filter(outlier_ind==1) %>%
+  group_by(key) %>%
+  dplyr::summarize(enc_cnt=length(unique(ENCOUNTERID)),
+                   low_bd=median(low_bd),
+                   low_bd_max=max(low_bd),
+                   up_bd=median(up_bd),
+                   up_bd=min(up_bd)) %>%
+  View  
 
 
 
 
-
+## quality checks
+#illogical dates (aki onsets before birth or after death)
+aki_bad_dates<-aki_stage_ind<-tbl1 %>%
+  dplyr::select(ENCOUNTERID,
+                AKI1_ONSET,AKI2_ONSET,AKI3_ONSET) %>%
+  gather(chk_pt, onset, -ENCOUNTERID) %>%
+  filter(!is.na(onset)) %>%
+  dplyr::select(ENCOUNTERID, chk_pt)%>%
+  mutate(chk_pt=gsub("_.*","",chk_pt))
