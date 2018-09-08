5a97ead5d62cc9e55993b5b2e160b6ee1b187596
xing song <sxingerl@gmail.com>
2018-09-07 20:12:46 -0500

    more debugging, but unlockBinding error still exists but can be ignored

--- render_report.R
+++ render_report.R
@@ -19,8 +19,12 @@ require_libraries(c("knitr",
 render_report(which_report="./report/AKI_CDM_EXT_VALID_p1_QA.Rmd",
               DBMS_type="Oracle")
 
+# The error: 
+# Error in unlockBinding("params", <environment>) : no binding for "params"
+# Can be ignored. It's because we need to delete "params" before the script is done.
+
 
 #TODO:
 #===============generate report for Part II=========================#
-# render_report(path_to_input="./report/AKI_CDM_EXT_VALID_p2_ModDev.Rmd",
+# render_report(path_to_input="./report/AKI_CDM_EXT_VALID_p2_ModelDev.Rmd",
 #               DBMS_type="Oracle")
