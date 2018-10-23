98854d21b951c48b165d7850bbd3d1285c5f9482
xing song <sxingerl@gmail.com>
2018-10-23 12:54:03 -0500

    corrected the logic of collecting first SCr within an encounter when matching by (patient, day)

--- inst/tSQL/cohort_enc_SCr.sql
+++ inst/tSQL/cohort_enc_SCr.sql
@@ -7,10 +7,11 @@
 /*
 /*action: write
 /********************************************************************************/
-select scr.* 
+select scr.*  
+      ,dense_rank() over (partition by PATID order by LAB_ORDER_DATE,SPECIMEN_DATE_TIME) rn
 into #AKI_Scr_eGFR
 from #All_Scr_eGFR scr
 where exists (select 1 from #AKI_Initial aki where scr.ENCOUNTERID = aki.ENCOUNTERID) or
-      exists (select 1 from #AKI_Initial aki where scr.PATID = aki.PATID and scr.LAB_ORDER_DATE between aki.ADMIT_DATE and aki.DISCHARGE_DATE)
+      exists (select 1 from #AKI_Initial aki where scr.PATID = aki.PATID and scr.LAB_ORDER_DATE between aki.ADMIT_DATE_TIME and aki.DISCHARGE_DATE)
 
 
