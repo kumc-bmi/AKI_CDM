08f50ef9eff16453271864e850509cfee99a7c12
xing song <sxingerl@gmail.com>
2020-05-18 22:40:19 -0500

    oracle script for med_admin collection

--- src/Oracle/collect_med.sql
+++ src/Oracle/collect_med.sql
@@ -12,29 +12,26 @@
 select distinct
        pat.PATID
       ,pat.ENCOUNTERID
-      ,to_date(to_char(trunc(p.RX_ORDER_DATE),'YYYY:MM:DD') || ' ' || to_char(p.RX_ORDER_TIME),
-               'YYYY:MM:DD HH24:MI') RX_ORDER_DATE_TIME
-      ,p.RX_START_DATE
-      ,least(pat.DISCHARGE_DATE,p.RX_END_DATE) RX_END_DATE
-      ,p.RX_BASIS
-      ,p.RXNORM_CUI
-      --,regexp_substr(p.RAW_RX_MED_NAME,'[^\[]+',1,1) RX_MED_NAME
-      ,p.RX_QUANTITY
-      --,p.RX_QUANTITY_UNIT
-      ,p.RX_REFILLS
-      ,p.RX_DAYS_SUPPLY
-      ,p.RX_FREQUENCY
-      ,case when p.RX_DAYS_SUPPLY > 0 and p.RX_QUANTITY is not null then round(p.RX_QUANTITY/p.RX_DAYS_SUPPLY) 
-            else null end as RX_QUANTITY_DAILY
-      ,round(p.RX_START_DATE-pat.ADMIT_DATE,2) DAYS_SINCE_ADMIT
+      ,to_date(to_char(trunc(m.MEDADMIN_START_DATE),'YYYY:MM:DD') || ' ' || coalesce(to_char(m.MEDADMIN_START_TIME),'00:00'),
+               'YYYY:MM:DD HH24:MI') MEDADMIN_START_DATE_TIME
+      ,to_date(to_char(trunc(m.MEDADMIN_STOP_DATE),'YYYY:MM:DD') || ' ' || coalesce(to_char(m.MEDADMIN_STOP_TIME),'00:00'),
+               'YYYY:MM:DD HH24:MI') MEDADMIN_STOP_DATE_TIME
+      ,m.MEDADMIN_TYPE
+      ,m.MEDADMIN_CODE
+      --,m.RAW_MEDADMIN_MED_NAME
+      ,m.MEDADMIN_DOSE_ADMIN
+      --,m.MEDADMIN_DOSE_ADMIN_UNIT
+      ,m.MEDADMIN_ROUTE
+      ,m.MEDADMIN_SOURCE
+      ,round(m.MEDADMIN_START_DATE-pat.ADMIT_DATE,2) DAYS_SINCE_ADMIT
 from AKI_onsets pat
-join &&cdm_db_schema.PRESCRIBING p
-on pat.PATID = p.PATID
-where p.RXNORM_CUI is not null and
-      p.RX_START_DATE is not null and
-      p.RX_ORDER_DATE is not null and 
-      p.RX_ORDER_TIME is not null and
-      p.RX_ORDER_DATE between pat.ADMIT_DATE-30 and coalesce(pat.AKI3_ONSET,pat.AKI2_ONSET,pat.AKI1_ONSET,pat.NONAKI_ANCHOR,pat.DISCHARGE_DATE)
-order by PATID, ENCOUNTERID, RXNORM_CUI, RX_START_DATE
+join &&cdm_db_schema.MED_ADMIN m
+on pat.PATID = m.PATID
+where m.MEDADMIN_CODE is not null and
+      m.MEDADMIN_START_DATE is not null and
+      --m.MEDADMIN_START_TIME is not null and 
+      m.MEDADMIN_STOP_DATE is not null and
+      --m.MEDADMIN_STOP_TIME is null and
+      m.MEDADMIN_START_DATE between pat.ADMIT_DATE-30 and coalesce(pat.AKI3_ONSET,pat.AKI2_ONSET,pat.AKI1_ONSET,pat.NONAKI_ANCHOR,pat.DISCHARGE_DATE)
 
 
